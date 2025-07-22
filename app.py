9f7a576d0b7124f8f14555c064a7c904d48eff20
Igor-Brumana <igorbrumanadelima@gmail.com>
2025-07-22 18:57:29 -0300

    adiciona

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template,request
+import csv
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/log-in", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        name= request.form.get("nome")
+        password = request.form.get("senha")
+
+        return f"Log in bem sucedido."
+
+    else:
+        return "Erro no log in."
+    
+
+@app.route("/sign-in", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        with open("usuarios.csv", mode="r", encoding="utf-8") as csv_file:
+            reader = list(csv.reader(csv_file))
+            last_id = 0
+
+            if len(reader) > 1:
+                last_row = reader[-1]
+                if last_row:
+                    last_id = int(last_row[0])
+
+            next_id = last_id + 1
+
+            novo_usuario = [
+            next_id,
+            request.form.get("nome"),
+            request.form.get("senha"),
+            request.form.get("nivel"),
+            request.form.get("preferences"),
+            ]
+
+        with open("usuarios.csv", mode="a", encoding="utf-8", newline="") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(novo_usuario)
+
+
+
+
+        return f"Log in bem sucedido."
+
+    else:
+        return "Erro no log in."
+    
+
+    
+if __name__ == "__main__":
+    app.run(debug = True) 
